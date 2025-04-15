0b9db6498a1892b003071bd8f4afc3eaaddbb79f
kwonsun212 <kwonsun212@gmail.com>
2025-04-15 15:47:38 +0900

    [Feat]캐릭터 대시 추가

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,33 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float jumpForce = 20f;
+    public float moveSpeed; //이동 속도
+    public float jumpForce = 10f;
     public Transform groundCheck;
     public LayerMask groundLayer;
     public Animator pAni;
+    public float sprint = 4f; // 대시 속도 배율
+    public float sprintDuration = 1f; // 달리기 지속 시간 (1초)
+    public float sprintCooldown = 5f; // 대시 쿨다운 시간 (5초)
 
     private Rigidbody2D rb;  
     private bool isGrounded;
+    private float currentSpeed = 5f; // 현재 이동 속도
+    private float sprintTimer; // 대시 타이머
+    private float sprintStart; // 대시 시작 시간
+    private bool isSprinting = false; //달리기 중인지 여부
+    private bool canSprint = true; // 대시 가능 여부
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
         pAni = GetComponent<Animator>();
         pAni.SetBool("move", false);
+        sprintTimer = 0f; // 타이머 초기화
+        currentSpeed = moveSpeed;
     }
    
     // Update is called once per frame
     void Update()
     {
         float moveInput = Input.GetAxisRaw("Horizontal");
+
+        if(!canSprint)
+        {
+            sprintTimer += Time.deltaTime;
+            if(sprintTimer >= sprintCooldown)
+            {
+                canSprint = true;
+                sprintTimer = 0f;
+
+            }
+        }
+
+        if(isSprinting && Time.time - sprintStart >= sprintDuration)
+        {
+            isSprinting = false;
+            pAni.SetBool("sprint", false);
+            moveSpeed = currentSpeed ;
+        }
+
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && canSprint)
+        {
+            isSprinting = true;
+            sprintStart = Time.time;
+            moveSpeed = currentSpeed  * sprint;
+            pAni.SetBool("sprint", true);
+            canSprint = false;
+        }
+        
+
         rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
 
-
+        //방향전환
         if (moveInput < 0)
             transform.localScale = new Vector3(1f, 1f, 1f);
         if (moveInput > 0)
@@ -56,6 +96,7 @@ public class PlayerController : MonoBehaviour
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             pAni.SetTrigger("JumpAction");
         }
+
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
