3c1ee6183f2b6907282d4aec189d05cbb48687a0
kwonsun212 <kwonsun212@gmail.com>
2025-04-17 01:14:22 +0900

    [Feat]다음부터 실수하지 말자

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -8,36 +8,38 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed; //�̵� �ӵ�
-    public float jumpForce = 10f; //�⺻ ���� ��
-    public float JumpIncrease = 4.0f; //���������� ������ ���� ��
-    public float JumpDuration = 5.0f; //���� ���� �ð�
+    public float moveSpeed; 
+    public float jumpForce = 10f; 
+    public float JumpIncrease = 4.0f; 
+    public float JumpDuration = 5.0f;
 
     private float originalJumpForce;
     private float boostTimer = 0f;
     private bool isBoosted = false;
 
-    public GameObject JumpTimerUI; //���� UI ������Ʈ
-    public TextMeshProUGUI JumpTimerText;     //���� UI ���� �ؽ�Ʈ 
+    public GameObject JumpTimerUI; 
+    public TextMeshProUGUI JumpTimerText;    
 
     public Transform groundCheck;
     public LayerMask groundLayer;
+
     public Animator pAni;
-    public float sprint = 4f; // ��� �ӵ� ����
-    public float sprintDuration = 1f; // �޸��� ���� �ð� (1��)
-    public float sprintCooldown = 5f; // ��� ��ٿ� �ð� (5��)
+    public float sprint = 4f;
+    public float sprintDuration = 1f; 
+    public float sprintCooldown = 5f;
 
     private Rigidbody2D rb;  
     private bool isGrounded;
-    private float currentSpeed = 5f; // ���� �̵� �ӵ�
-    private float sprintTimer; // ��� Ÿ�̸�
-    private float sprintStart; // ��� ���� �ð�
-    private bool isSprinting = false; //�޸��� ������ ����
-    private bool canSprint = true; // ��� ���� ����
 
-    [Header("���� ���� ����")]
-    public float falMultiplier = 2.5f;          //�ϰ� �߷� ����
-    public float lowJumpMultiplier = 2.0f;      //ª�� ���� ����
+    private float currentSpeed = 5f; 
+    private float sprintTimer; 
+    private float sprintStart; 
+    private bool isSprinting = false; 
+    private bool canSprint = true; 
+
+    [Header("점프 개선 설정")]
+    public float falMultiplier = 2.5f;          
+    public float lowJumpMultiplier = 2.0f;     
 
 
 
@@ -46,7 +48,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         pAni = GetComponent<Animator>();
         pAni.SetBool("move", false);
-        sprintTimer = 0f; // Ÿ�̸� �ʱ�ȭ
+        sprintTimer = 0f;
         currentSpeed = moveSpeed;
 
         originalJumpForce = jumpForce;
@@ -54,7 +56,7 @@ public class PlayerController : MonoBehaviour
         if (JumpTimerUI != null)
             JumpTimerUI.SetActive(false);
     }
-    
+
 
     // Update is called once per frame
     void Update()
@@ -65,116 +67,116 @@ public class PlayerController : MonoBehaviour
 
             float timeLeft = Mathf.Clamp(JumpDuration - boostTimer, 0f, JumpDuration);
             if (JumpTimerText != null)
+            {
                 JumpTimerText.text = timeLeft.ToString("F1");
-
-
+            }
 
             if (boostTimer >= JumpDuration)
             {
-                jumpForce = originalJumpForce; // ������ ������� ����
-                isBoosted = false;
+                jumpForce = originalJumpForce;
                 boostTimer = 0f;
 
                 if (JumpTimerUI != null)
                     JumpTimerUI.SetActive(false);
 
-
+                isBoosted = false;
             }
 
-        //������ �Է�
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-
-        //�̵� ���� ����
-        Vector2 movement = new Vector3(moveHorizontal, 0, moveVertical);    //�̵� ���� ����
-
-        //�ӵ��� ���� �̵�
-        rb.velocity = new Vector3(moveHorizontal * moveSpeed, rb.velocity.y, moveVertical * moveSpeed);
-
-        //���� ���� ���� ����
-        if (rb.velocity.y < 0)
-        {
-            //�ϰ� �� �߷� ��ȭ
-            rb.velocity += Vector2.up * Physics.gravity.y * (falMultiplier - 1) * Time.deltaTime; //�ϰ� �� �߷� ��ȭ
         }
-        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
-        {
-            rb.velocity += Vector2.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime; // ����� ���� ��ư�� ���� ���� ����
-
-        }
+            float moveHorizontal = Input.GetAxis("Horizontal");
+            float moveVertical = Input.GetAxis("Vertical");
 
 
+            Vector2 movement = new Vector3(moveHorizontal, 0, moveVertical);
 
-        float moveInput = Input.GetAxisRaw("Horizontal");
 
-        if(!canSprint)
-        {
-            sprintTimer += Time.deltaTime;
-            if(sprintTimer >= sprintCooldown)
+            rb.velocity = new Vector3(moveHorizontal * moveSpeed, rb.velocity.y, moveVertical * moveSpeed);
+
+
+            if (rb.velocity.y < 0)
             {
-                canSprint = true;
-                sprintTimer = 0f;
+
+                rb.velocity += Vector2.up * Physics.gravity.y * (falMultiplier - 1) * Time.deltaTime;
+            }
+            else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+            {
+                rb.velocity += Vector2.up * Physics.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
 
             }
-        }
 
-        if(isSprinting && Time.time - sprintStart >= sprintDuration)
-        {
-            isSprinting = false;
-            pAni.SetBool("sprint", false);
-            moveSpeed = currentSpeed ;
-        }
 
-        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && canSprint)
-        {
-            isSprinting = true;
-            sprintStart = Time.time;
-            moveSpeed = currentSpeed  * sprint;
-            pAni.SetBool("sprint", true);
-            canSprint = false;
-        }
+
+            float moveInput = Input.GetAxisRaw("Horizontal");
+
+            if (!canSprint)
+            {
+                sprintTimer += Time.deltaTime;
+                if (sprintTimer >= sprintCooldown)
+                {
+                    canSprint = true;
+                    sprintTimer = 0f;
+
+                }
+            }
+
+            if (isSprinting && Time.time - sprintStart >= sprintDuration)
+            {
+                isSprinting = false;
+                pAni.SetBool("sprint", false);
+                moveSpeed = currentSpeed;
+            }
+
+            if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && canSprint)
+            {
+                isSprinting = true;
+                sprintStart = Time.time;
+                moveSpeed = currentSpeed * sprint;
+                pAni.SetBool("sprint", true);
+                canSprint = false;
+            }
+
+
+            rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+            
+            transform.Translate(new Vector3(Mathf.Abs(moveInput) * Time.deltaTime, 0, 0));
+            if (moveInput > 0)
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+            }
+            else if (moveInput < 0)
+            {
+                transform.eulerAngles = new Vector3(0, 180, 0);
+            }
+
+
+            if (moveInput < 0)
+            {
+                pAni.SetBool("move", true);
+            }
+            else if (moveInput > 0)
+            {
+                pAni.SetBool("move", true);
+
+            }
+            else
+            {
+                pAni.SetBool("move", false);
+            }
+
+
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+
+            if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                pAni.SetTrigger("JumpAction");
+            }
+
         
-
-        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
-
-        //������ȯ
-        transform.Translate(new Vector3(Mathf.Abs(moveInput) * Time.deltaTime, 0, 0));
-        if(moveInput > 0)
-        {
-            transform.eulerAngles = new Vector3(0, 0, 0);
-        }
-        else if(moveInput < 0)
-        {
-            transform.eulerAngles = new Vector3(0, 180, 0);
-        }
-
-
-        if (moveInput < 0)
-        {
-            pAni.SetBool("move", true);
-        }
-        else if (moveInput > 0)
-        {
-            pAni.SetBool("move", true);
-
-        }
-        else
-        {
-            pAni.SetBool("move", false);
-        }
-         
-           
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-
-        if(isGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-            pAni.SetTrigger("JumpAction");
-        }
-
-
-       
     }
+       
+    
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.CompareTag("Respawn"))
